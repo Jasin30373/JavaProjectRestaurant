f008dc2cf4320c5a0f3f38405a0e3b01a053d7c1
Arif Arifi <aa30253@seeu.edu.mk>
2024-01-26 15:09:28 +0100

    Entity DONE !!

--- src/main/java/com/example/ProjectJavaRestaurant/pojo/entity/Customer_order.java
+++ src/main/java/com/example/ProjectJavaRestaurant/pojo/entity/Customer_order.java
@@ -1,4 +1,64 @@
 package com.example.ProjectJavaRestaurant.pojo.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
 public class Customer_order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToMany
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToMany
+    @JoinTable(name = "order_menu_items",
+                joinColumns = @JoinColumn(name = "order_id"),
+                inverseJoinColumns = @JoinColumn(name = "menu_item_id"))
+    private List<MenuItem> items;
+    private double totalAmount;
+
+    public Customer_order(){
+    }
+
+    public Customer_order(Integer id, Customer customer, List<MenuItem> items, double totalAmount) {
+        this.id = id;
+        this.customer = customer;
+        this.items = items;
+        this.totalAmount = totalAmount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<MenuItem> items) {
+        this.items = items;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
--- src/main/java/com/example/ProjectJavaRestaurant/pojo/entity/MenuItem.java
+++ src/main/java/com/example/ProjectJavaRestaurant/pojo/entity/MenuItem.java
@@ -1,4 +1,36 @@
 package com.example.ProjectJavaRestaurant.pojo.entity;
 
-public class MenuItem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity public class MenuItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private double price;
+
+    public MenuItem(){
+
+    }
+
+    public MenuItem(Integer id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    public Integer getId() {return id;}
+
+    public void setId(Integer id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public double getPrice() {return price;}
+
+    public void setPrice(double price) {this.price = price;}
+
 }
