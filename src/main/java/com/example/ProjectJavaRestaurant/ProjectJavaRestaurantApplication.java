d831aec727408b17fdcd4165f02368a23e916ac0
Jasin Ismaili <ji30373@seeu.edu.mk>
2024-01-26 13:37:46 +0100

    init

---
+++ src/main/java/com/example/ProjectJavaRestaurant/ProjectJavaRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectJavaRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaRestaurantApplication.class, args);
+	}
+
+}
